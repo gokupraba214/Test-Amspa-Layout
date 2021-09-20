b8252cb6f4d97117f2ad06e421c9a60ccb0b2a55
gokupraba214 <88436895+gokupraba214@users.noreply.github.com>
2021-09-20 18:28:41 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+
+<!--- Footer -->
+<footer class="footer-area bg-f">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-md-6" >
+				<h3><a href="index.php"><img src="images/AMSPA.png" alt="AMSPA" style="height: 60px; margin-top: 5%; margin-left: -15px;" ></h3></a>
+                <br><h4 class="h4" style="text-align: left; margin-bottom: 20px;margin-top:-20px;"> FREE E-BOOK</h4>
+			  <a href="https://online.pubhtml5.com/xvgw/iuop/" target="blank" style="margin-left: 0%;"><img src="images/Ebook/amspa.JPG"  alt="amspa qr"></a><br><br>
+			  <p><a style="color: white; font-size: 16px; margin-left: -1%;" href=""target="blank">For advertising & enquries</p></a>
+			  
+		  
+			 
+			</div>
+			
+			<div class="footer_guides_menu">
+				<h3 class="footer_headers" style="text-align: left;margin-left:22%;">Our Location </h3> 
+				<p class="p" style="text-align: left;margin-left: 100px;">
+					No.31-2, Block F2, Level 2,<br>
+					Jalan PJU 1/42a, Dataran Prima,<br>
+					47301 Petaling Jaya, Selangor<br>
+					<p style="text-align: left;margin-left: 100px;">Opening Hours:<br><span class="text-color">9.00am - 6.00pm (Mon-Fri)</p>
+			  </div>
+				<p class="p">
+				</p>
+				
+			<div class="col-lg-3 col-md-6" style="margin-left: 6%;" >
+				<h3 style="text-align: left;margin-top: 8%;">Contact Information</h3>
+				<p class="p" style="text-align: left;margin-top: 7%;">
+				Mobile No: +6012-2200622<br>Tel No: +603-78869219
+				<a style="color: white" href="mailto:enquiry@bluedale.com.my">enquiry@bluedale.com.my</a>
+				</p>
+			
+			</div>
+				
+			</div>	
+
+			<br>
+			<div class="col-lg-12">
+				<p class="company-name" style="text-align: center;"> ©2021. Bluedale Group Of Companies. All Rights Reserved.
+					Designed, developed &amp; maintained by Bluedale Group Of Companies 
+					<div style="text-align: center;">
+						<a href="pdpa.php" target="_blank">
+						  <span style="color:white">PRIVACY POLICY</span>
+						</a>
+					  </div>
+			
+			</div>
+		</div>
+	</div>
+	
+
+	
+	
+  </footer>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+
+<header class="top-navbar">
+	<nav class="navbar navbar-expand-xl navbar-light bg-light">
+		<div class="container">
+			<a class="navbar-brand" href="index.php">
+				<img src="images/AMSPA.png" width="140px" alt="" />
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food"
+				aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbars-rs-food">
+				<ul class="navbar-nav ml-auto header_tabs">
+					<li class="<?php if($page=='home'){echo 'nav-item active';}?>"><a class="nav-link" href="index.php">Home</a></li>
+					<li class="<?php if($page=='aboutus'){echo 'nav-item active';}?>"><a class="nav-link" href="about.php">About Us</a></li>
+					<li class="<?php if($page=='ebook'){echo 'nav-item active';}?>"><a class="nav-link" href="ebook.php">E-book</a></li>
+					<li class="<?php if($page=='contactus'){echo 'nav-item active';}?>"><a class="nav-link" href="contact.php">Contact Us</a></li>
+
+				</ul>
+
+			</div>
+			
+		</div>
+	</nav>
+</header>
