26bfc0097ae233e083001b56c7dbdf7f779e1825
gokupraba214 <88436895+gokupraba214@users.noreply.github.com>
2021-09-20 18:26:52 +0800

    Add files via upload

---
+++ js/read_more.js
@@ -0,0 +1,9 @@
+function openReadMore(x,y) {
+    document.getElementById(x).style.display = "block";
+    document.getElementById(y).style.display = "none";
+  }
+  
+  function closeReadMore(x,y) {
+    document.getElementById(x).style.display = "none";
+    document.getElementById(y).style.display = "block";
+  } 
